3ff4f03857dafeba1ff389bdd9b8f949361f5492
rpawlaczyk <189746@edu.p.lodz.pl>
2016-04-24 17:37:29 +0200

    gui dla mok0

---
+++ src/main/java/pl/lodz/p/it/ssbd2016/ssbd01/mok/beans/Logowanie.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.p.it.ssbd2016.ssbd01.mok.beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedProperty;
+import javax.inject.Inject;
+import javax.inject.Named;
+import pl.lodz.p.it.ssbd2016.ssbd01.encje.Konto;
+/**
+ *
+ * @author rpawlaczyk
+ */
+@Named
+@RequestScoped
+public class Logowanie {
+
+    private String login;
+    private String haslo;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getHaslo() {
+        return haslo;
+    }
+
+    public void setHaslo(String haslo) {
+        this.haslo = haslo;
+    }
+    
+    public String zalogujKlienta(){
+        if(false!=false)
+            return "errorLogin";
+        else
+            return "zalogowano";
+    }
+
+}
