5d06039a5224b564c58446027bfeca887a079f09
zmax <>
2016-04-25 07:41:23 +0200

    Przypadek uzycia MOK1, MOK2, MOK12, MOK13, MOK15. Brak walidacji

--- src/main/java/pl/lodz/p/it/ssbd2016/ssbd01/mok/beans/WyszukajKontaNiepotwierdzone.java
+++ src/main/java/pl/lodz/p/it/ssbd2016/ssbd01/mok/beans/WyszukajKontaNiepotwierdzone.java
@@ -3,24 +3,28 @@ package pl.lodz.p.it.ssbd2016.ssbd01.mok.beans;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.bean.ManagedBean;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import javax.inject.Inject;
 import javax.inject.Named;
 import pl.lodz.p.it.ssbd2016.ssbd01.encje.Konto;
 
-@Named
-@RequestScoped
+@ManagedBean(name="wyszukajKontaNiepotwierdzone")
+@ViewScoped
 public class WyszukajKontaNiepotwierdzone {
     @Inject
     private UzytkownikSession uzytkownikSession;
     
-    private DataModel<Konto> kontaNiepotwierdzoneWyszukiwanie;
+    private ArrayList<Konto> kontaNiepotwierdzoneWyszukiwanie = new ArrayList();
     private String parametr;
     private String wartoscParametru;
+    private boolean first = true;
     
-    public DataModel<Konto> getKontaNiepotwierdzoneWyszukiwanie() {
+    public ArrayList<Konto> getKontaNiepotwierdzoneWyszukiwanie() {
         return kontaNiepotwierdzoneWyszukiwanie;
     }
     
@@ -40,47 +44,52 @@ public class WyszukajKontaNiepotwierdzone {
     
     @PostConstruct
     public void init() {
-        this.kontaNiepotwierdzoneWyszukiwanie = new ListDataModel<Konto>();
+        List<Konto> temp = uzytkownikSession.pobierzWszystkieKonta();
+        kontaNiepotwierdzoneWyszukiwanie = new ArrayList<Konto>();
+        
+        for(int i = 0; i < temp.size(); i++)
+        {
+            if(temp.get(i).getPotwierdzone() == false)
+                kontaNiepotwierdzoneWyszukiwanie.add(temp.get(i));
+        }
     }
     
     public void initWyszukiwanie() {
         List<Konto> temp = uzytkownikSession.pobierzWszystkieKonta();
-        List<Konto> konta = new ArrayList<Konto>();
+        kontaNiepotwierdzoneWyszukiwanie = new ArrayList<Konto>();
+        
         for(int i = 0; i < temp.size(); i++)
         {
             if(parametr.equals("login") == true)
             {
                 if(temp.get(i).getPotwierdzone() == false && temp.get(i).getLogin().equals(wartoscParametru) == true)
-                    konta.add(temp.get(i));
+                    kontaNiepotwierdzoneWyszukiwanie.add(temp.get(i));
             }
             else if(parametr.equals("email") == true)
             {
                 if(temp.get(i).getPotwierdzone() == false && temp.get(i).getEmail().equals(wartoscParametru) == true)
-                    konta.add(temp.get(i));
+                    kontaNiepotwierdzoneWyszukiwanie.add(temp.get(i));
             }
             else if(parametr.equals("telefon") == true)
             {
                 if(temp.get(i).getPotwierdzone() == false && temp.get(i).getTelefon().equals(wartoscParametru) == true)
-                    konta.add(temp.get(i));
+                    kontaNiepotwierdzoneWyszukiwanie.add(temp.get(i));
             }
             else if(parametr.equals("nazwisko") == true)
             {
                 if(temp.get(i).getPotwierdzone() == false && temp.get(i).getNazwisko().equals(wartoscParametru) == true)
-                    konta.add(temp.get(i));
+                    kontaNiepotwierdzoneWyszukiwanie.add(temp.get(i));
             }
             else if(parametr.equals("id") == true)
             {
                 if(temp.get(i).getPotwierdzone() == false && temp.get(i).getId().toString().equals(wartoscParametru) == true)
-                    konta.add(temp.get(i));
+                    kontaNiepotwierdzoneWyszukiwanie.add(temp.get(i));
             }
-
         }
-        kontaNiepotwierdzoneWyszukiwanie = new ListDataModel<Konto>(konta);
     }
     
-    public void potwierdzKontoWyszukiwanie() {
-        uzytkownikSession.potwierdzKonto(kontaNiepotwierdzoneWyszukiwanie.getRowData());
-        System.out.println(kontaNiepotwierdzoneWyszukiwanie.getRowData());
+    public void potwierdzKontoWyszukiwanie(Konto k) {
+        uzytkownikSession.potwierdzKonto(k);
         initWyszukiwanie();
     }
 }
