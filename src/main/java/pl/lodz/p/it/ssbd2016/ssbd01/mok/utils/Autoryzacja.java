3c7a5ca98dc8abd6fa96a6edc41ef044919b9b82
Patryk <Patryk@localhost>
2016-05-05 12:42:47 +0200

    autoryzacja, widocznosc przyciskow

--- src/main/java/pl/lodz/p/it/ssbd2016/ssbd01/mok/utils/Autoryzacja.java
+++ src/main/java/pl/lodz/p/it/ssbd2016/ssbd01/mok/utils/Autoryzacja.java
@@ -5,9 +5,17 @@
  */
 package pl.lodz.p.it.ssbd2016.ssbd01.mok.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /**
  *
@@ -17,9 +25,25 @@ import javax.inject.Named;
 @RequestScoped
 public class Autoryzacja {
     
+    
     public boolean czyAdministrator()
     {
-        return sprawdzRole("ADMINISTRATOR");
+        return sprawdzRole(PoziomDostepuManager.getPoziomyDostepu().get(0));
+    }
+    
+    public boolean czyAgent()
+    {
+        return sprawdzRole(PoziomDostepuManager.getPoziomyDostepu().get(2));
+    }
+    
+    public boolean czyKlient()
+    {
+        return sprawdzRole(PoziomDostepuManager.getPoziomyDostepu().get(3));
+    }
+    
+    public boolean czyMenadzer()
+    {
+        return sprawdzRole(PoziomDostepuManager.getPoziomyDostepu().get(1));
     }
     
     private boolean sprawdzRole(String role) {
