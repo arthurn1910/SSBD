a3a3c9715589eae8da8697ceb32c64e8a94a89a8
gleam <189660@edu.p.lodz.pl>
2016-05-16 13:42:38 +0200

    SSBD201601-141 dodano brakujące wywołania fasad w OgloszenieManager

--- src/main/java/pl/lodz/p/it/ssbd2016/ssbd01/moo/managers/OgloszenieManager.java
+++ src/main/java/pl/lodz/p/it/ssbd2016/ssbd01/moo/managers/OgloszenieManager.java
@@ -1,12 +1,16 @@
 package pl.lodz.p.it.ssbd2016.ssbd01.moo.managers;
 
 import javax.annotation.security.RolesAllowed;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.interceptor.Interceptors;
+import pl.lodz.p.it.ssbd2016.ssbd01.encje.Konto;
 import pl.lodz.p.it.ssbd2016.ssbd01.encje.Ogloszenie;
 import pl.lodz.p.it.ssbd2016.ssbd01.interceptors.TrackerInterceptor;
+import pl.lodz.p.it.ssbd2016.ssbd01.moo.fasady.KontoMOOFacadeLocal;
+import pl.lodz.p.it.ssbd2016.ssbd01.moo.fasady.OgloszenieFacadeLocal;
 
 /**
  * Klasa pośrednicząca miedzy MOOEndpoint a fasadami. Przetwarza niezbędne dane.
@@ -17,16 +21,30 @@ import pl.lodz.p.it.ssbd2016.ssbd01.interceptors.TrackerInterceptor;
 @TransactionAttribute(TransactionAttributeType.MANDATORY)
 public class OgloszenieManager implements OgloszenieManagerLocal {
 
+    
+    @EJB
+    private KontoMOOFacadeLocal kontoFacade;
+    @EJB
+    private OgloszenieFacadeLocal ogloszenieFacadeLocal;
+    
     @Override
     @RolesAllowed("dodajDoUlubionych")
     public void dodajDoUlubionych(Ogloszenie ogloszenie) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Konto konto = kontoFacade.znajdzPoLoginie("");
+        Ogloszenie ogloszenieTemp = ogloszenieFacadeLocal.znajdzPoID(ogloszenie.getId());
+        // Czynności związane z logiką
+        ogloszenieFacadeLocal.edit(ogloszenieTemp);
+        kontoFacade.edit(konto);
     }
 
     @Override
     @RolesAllowed("usunZUlubionych")
     public void usunZUlubionych(Ogloszenie ogloszenie) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Konto konto = kontoFacade.znajdzPoLoginie("");
+        Ogloszenie ogloszenieTemp = ogloszenieFacadeLocal.znajdzPoID(ogloszenie.getId());
+        // Czynności związane z logiką
+        ogloszenieFacadeLocal.edit(ogloszenieTemp);
+        kontoFacade.edit(konto);
     }
     
 }
