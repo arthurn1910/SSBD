76aa04c99d61f39507879f50adb0bdd120ef8f31
Kamil Rogowski <189760@edu.p.lodz.pl>
2016-06-09 09:43:26 +0200

    Integracja przypadku wyswietl spotkania dla ogloszenia ze szczegolami ogloszenia

--- src/main/java/pl/lodz/p/it/ssbd2016/ssbd01/mos/beans/WyswietlSpotkaniaDlaOgloszeniaBean.java
+++ src/main/java/pl/lodz/p/it/ssbd2016/ssbd01/mos/beans/WyswietlSpotkaniaDlaOgloszeniaBean.java
@@ -2,12 +2,21 @@ package pl.lodz.p.it.ssbd2016.ssbd01.mos.beans;
 
 import pl.lodz.p.it.ssbd2016.ssbd01.encje.Ogloszenie;
 import pl.lodz.p.it.ssbd2016.ssbd01.encje.Spotkanie;
+import pl.lodz.p.it.ssbd2016.ssbd01.mok.beans.HistoriaLogowaniaRaportBean;
+import pl.lodz.p.it.ssbd2016.ssbd01.moo.beans.OgloszenieSession;
+import pl.lodz.p.it.ssbd2016.ssbd01.wyjatki.WyjatekSystemu;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Obsługuje widok spotkań dla ogłoszenia
@@ -26,13 +35,32 @@ public class WyswietlSpotkaniaDlaOgloszeniaBean {
     @Inject
     private SpotkanieSession spotkanieSession;
 
+    @Inject
+    private OgloszenieSession ogloszenieSession;
+
     /**
      * inicjalizacja modelu danych, MOS.5, Kamil Rogowski
      */
     @PostConstruct
     public void init() {
-        ogloszenie = spotkanieSession.pobierzWybraneOgloszenie();
-        spotkania = spotkanieSession.pobierzSpotkaniaDlaOgloszenia(ogloszenie);
+
+        try {
+            ogloszenie = ogloszenieSession.getOgloszenieDoWyswietlenia();
+            spotkania = spotkanieSession.pobierzSpotkaniaDlaOgloszenia(ogloszenie);
+        } catch (WyjatekSystemu ex) {
+            Logger lg = Logger.getLogger("javax.enterprice.system.conteiner.web.faces");
+            ex.setMessage("blad.NullPointerException");
+            spotkanieSession.setException(ex);
+            lg.log(Level.SEVERE, this.getClass() + ": Wystąpił wyjątek: ", ex);
+            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+            HttpServletRequest origRequest = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+            try {
+                externalContext.redirect(origRequest.getContextPath() + "/wyjatki/wyjatekMOS.xhtml");
+            } catch (IOException ex1) {
+                Logger.getLogger(HistoriaLogowaniaRaportBean.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        }
+
     }
 
     public Ogloszenie getOgloszenie() {
